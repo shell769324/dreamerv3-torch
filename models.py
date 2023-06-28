77e298d4f3584b2e6ae71549bdbe28960120c2fd
shell769324 <bobbyzenpai@gmail.com>
2023-06-27 23:09:43 -0400

    remove print

--- models.py
+++ models.py
@@ -398,8 +398,8 @@ class ImagBehavior(nn.Module):
             feat = dynamics.get_feat(state)
             feat = torch.cat([feat, target_onehot], -1)
             inp = feat.detach() if self._stop_grad_actor else feat
-            print(inp)
-            action = policy(inp).sample()
+            print(inp.shape, inp)
+            action = policy(inp.double()).sample()
             succ = dynamics.img_step(state, action, sample=self._config.imag_sample)
             return succ, feat, action
 
