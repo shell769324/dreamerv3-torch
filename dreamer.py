28c39e7c754fb26d15b4ea0780eb2b0c5bd25e57
shell769324 <bobbyzenpai@gmail.com>
2023-06-27 22:00:10 -0400

    correct

--- dreamer.py
+++ dreamer.py
@@ -130,9 +130,9 @@ class Dreamer(nn.Module):
         if self._config.eval_state_mean:
             latent["stoch"] = latent["mean"]
         target_onehot = torch.zeros((len(obs["image"])), len(targets)).to(self._config.device)
-        print(info)
+        print("info", info)
         for i, inf in enumerate(info):
-            print(inf)
+            print("inf", inf)
             target_onehot[i] = inf["target"].to(self._config.device)
         feat = self._wm.dynamics.get_feat(latent)
         feat = torch.cat([feat, target_onehot], -1)
