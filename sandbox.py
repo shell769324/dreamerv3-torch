9f637ef37725b2a09020152d4dc0e3eaa6255b98
shell769324 <bobbyzenpai@gmail.com>
2023-07-11 23:08:10 -0400

    go

---
+++ sandbox.py
@@ -0,0 +1,127 @@
+import sys
+import ruamel.yaml as yaml
+import argparse
+import pathlib
+import numpy as np
+import torch
+import models
+import tools
+import envs.wrappers as wrappers
+from dreamer import Dreamer
+
+def make_env(config, logger, mode, train_eps, eval_eps):
+    # crafter_reward
+    # [crafter, reward]
+    suite, task = config.task.split("_", 1)
+    if suite == "dmc":
+        import envs.dmc as dmc
+
+        env = dmc.DeepMindControl(task, config.action_repeat, config.size)
+        env = wrappers.NormalizeActions(env)
+    elif suite == "atari":
+        import envs.atari as atari
+
+        env = atari.Atari(
+            task,
+            config.action_repeat,
+            config.size,
+            gray=config.grayscale,
+            noops=config.noops,
+            lives=config.lives,
+            sticky=config.stickey,
+            actions=config.actions,
+            resize=config.resize,
+        )
+        env = wrappers.OneHotAction(env)
+    elif suite == "dmlab":
+        import envs.dmlab as dmlab
+
+        env = dmlab.DeepMindLabyrinth(
+            task, mode if "train" in mode else "test", config.action_repeat
+        )
+        env = wrappers.OneHotAction(env)
+    elif suite == "crafter":
+        import envs.crafter as crafter
+        env = crafter.Crafter(
+            task, outdir="./stats"
+        )
+        env = wrappers.OneHotAction(env)
+    else:
+        raise NotImplementedError(suite)
+    env = wrappers.TimeLimit(env, config.time_limit)
+    env = wrappers.SelectAction(env, key="action")
+    env = wrappers.RewardObs(env)
+    return env
+
+
+def make_dataset(episodes, config):
+    generator = tools.sample_episodes(episodes, config.batch_length)
+    dataset = tools.from_generator(generator, config.batch_size)
+    return dataset
+
+def main(config, defaults):
+    logdir = pathlib.Path(config.logdir).expanduser()
+    config.traindir = config.traindir or logdir / "train_eps"
+    config.evaldir = config.evaldir or logdir / "eval_eps"
+    config.steps //= config.action_repeat
+    config.eval_every //= config.action_repeat
+    config.log_every //= config.action_repeat
+    config.time_limit //= config.action_repeat
+    config.act = getattr(torch.nn, config.act)
+    config.norm = getattr(torch.nn, config.norm)
+
+    print("Logdir", logdir)
+    logdir.mkdir(parents=True, exist_ok=True)
+    config.traindir.mkdir(parents=True, exist_ok=True)
+    config.evaldir.mkdir(parents=True, exist_ok=True)
+    logger = tools.Logger(logdir, 0)
+
+    print("Create envs.")
+    if config.offline_traindir:
+        directory = config.offline_traindir.format(**vars(config))
+    else:
+        directory = config.traindir
+    train_eps = tools.load_episodes(directory, limit=config.dataset_size)
+    if config.offline_evaldir:
+        directory = config.offline_evaldir.format(**vars(config))
+    else:
+        directory = config.evaldir
+    eval_eps = tools.load_episodes(directory, limit=1)
+    make = lambda mode: make_env(config, logger, mode, train_eps, eval_eps)
+    train_envs = [make("train") for _ in range(config.envs)]
+    eval_envs = [make("eval") for _ in range(config.envs)]
+    acts = train_envs[0].action_space
+    config.num_actions = acts.n if hasattr(acts, "n") else acts.shape[0]
+
+    print("Simulate agent.")
+    train_dataset = make_dataset(train_eps, config)
+    eval_dataset = make_dataset(eval_eps, config)
+    agent = Dreamer(config, logger, train_dataset).to(config.device)
+    agent.requires_grad_(requires_grad=False)
+    if (logdir / "latest_model.pt").exists():
+        agent.load_state_dict(torch.load(logdir / "latest_model.pt"))
+        agent._should_pretrain._once = False
+    embeddings = {}
+    for i in range(6):
+        embeddings[i] = agent._wm.embedding(torch.IntTensor([i]))
+    for i in range(6):
+        for j in range(6):
+            print(torch.nn.functional.cosine_similarity(embeddings[i], embeddings[j]), end=" ")
+        print("")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--configs", nargs="+", required=True)
+    args, remaining = parser.parse_known_args()
+    configs = yaml.safe_load(
+        (pathlib.Path(sys.argv[0]).parent / "configs.yaml").read_text()
+    )
+    defaults = {}
+    for name in args.configs:
+        defaults.update(configs[name])
+    parser = argparse.ArgumentParser()
+    for key, value in sorted(defaults.items(), key=lambda x: x[0]):
+        arg_type = tools.args_type(value)
+        parser.add_argument(f"--{key}", type=arg_type, default=arg_type(value))
+    main(parser.parse_args(remaining), defaults)
