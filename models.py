d78cb923f6344152ffcef40a3525d813d27d2108
shell769324 <bobbyzenpai@gmail.com>
2023-08-07 12:46:28 -0400

    go

--- models.py
+++ models.py
@@ -142,7 +142,7 @@ class WorldModel(nn.Module):
 
     def _train(self, data):
         torch.set_printoptions(precision=3, sci_mode=False)
-        np.set_printoptions(precision=3)
+        np.set_printoptions(precision=3, suppress=True)
         # action (batch_size, batch_length, act_dim)
         # image (batch_size, batch_length, h, w, ch)
         # reward (batch_size, batch_length)
