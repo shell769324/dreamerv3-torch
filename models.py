a54973a34c7329a8c53977df814befc839dd1c61
shell769324 <bobbyzenpai@gmail.com>
2023-08-07 12:15:57 -0400

    go

--- models.py
+++ models.py
@@ -168,7 +168,7 @@ class WorldModel(nn.Module):
                         stoch, deter = (stoch, deter) if grad_head else (stoch.detach(), deter.detach())
                         pred = head(stoch, deter, data["target"])
                         print(pred.mean().shape, data[name].shape)
-                        print(pred.mean().squeeze(-1)[2:5, 10:40], data[name].squeeze(-1)[2:5, 10:40])
+                        print(pred.mean().squeeze(-1)[2:6, 20:40].item(), data[name].squeeze(-1)[2:6, 20:40].item())
                         exit(1)
                     else:
                         feat = self.dynamics.get_feat(post)
