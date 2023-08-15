0c784261169ec022b665e0d98473802f10d85b71
shell769324 <bobbyzenpai@gmail.com>
2023-08-15 10:03:58 -0400

    go

--- AttentionNetworks.py
+++ AttentionNetworks.py
@@ -36,6 +36,8 @@ class FeedForward(nn.Module):
 
     def forward(self, x):
         x, q2 = x
+        print("x linear", x.abs().mean())
+        print("q2", q2.abs().mean())
         return self.net(x) + x, self.net2(q2) + q2
 
 
@@ -60,6 +62,8 @@ class Attention(nn.Module):
         x, qoir = x
         q2 = rearrange(qoir, 'b n (h d) -> b h n d', h=self.heads)
         qkv = self.to_qkv(x).chunk(3, dim=-1)
+        print("x", x.abs().mean())
+        print("qoir", qoir.abs().mean())
         q, k, v = map(lambda t: rearrange(t, 'b n (h d) -> b h n d', h=self.heads), qkv)
         dots = torch.matmul(q, k.transpose(-1, -2)) * self.scale
         # b h 1 d
