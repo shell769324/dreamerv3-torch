c5ee70a95c5980e3e4749a64a3c062e9bea39f53
shell769324 <bobbyzenpai@gmail.com>
2023-08-14 23:37:18 -0400

    go

--- dreamer.py
+++ dreamer.py
@@ -163,7 +163,7 @@ class Dreamer(nn.Module):
         action = self._exploration(action, training)
         policy_output = {"action": action, "logprob": logprob}
         state = (latent, action)
-        print(tools.TwoHotDistSymlog(logits=value_params, device=self._device).mode())
+        print(tools.TwoHotDistSymlog(logits=value_params).mode())
         exit(1)
         return policy_output, state, tools.TwoHotDistSymlog(logits=value_params, device=self._device).mode()
 
