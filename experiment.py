426a780733af1ba8901b29da8da9fba970998ae0
shell769324 <bobbyzenpai@gmail.com>
2023-08-18 00:09:06 -0400

    go

--- experiment.py
+++ experiment.py
@@ -9,13 +9,13 @@ dist = TwoHotDistSymlog(logits, device="cuda", buckets=10, low=-3, high=3)
 
 for i in range(200):
     if i % 8 == 0:
-        ground = torch.tensor([-0.5])
+        ground = torch.tensor([-0.5]).to("cuda")
     elif i % 23 == 1:
-        ground = torch.tensor([1])
+        ground = torch.tensor([1]).to("cuda")
     elif i % 8 == 2:
-        ground = torch.tensor([0.5])
+        ground = torch.tensor([0.5]).to("cuda")
     else:
-        ground = torch.tensor([0])
+        ground = torch.tensor([0]).to("cuda")
     loss = -dist.log_prob(ground).mean()
     loss.backward()
     opt.step()
