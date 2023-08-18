0ea08dbc4aeae16da6b2944aa65932f078186607
shell769324 <bobbyzenpai@gmail.com>
2023-08-18 00:11:52 -0400

    go

--- experiment.py
+++ experiment.py
@@ -6,10 +6,10 @@ torch.set_printoptions(linewidth=180)
 logits = [-0.0] * 10
 logits = torch.tensor(logits).to("cuda")
 logits.requires_grad = True
-opt = torch.optim.AdamW([logits])
+opt = torch.optim.AdamW([logits], lr=3e-5)
 dist = TwoHotDistSymlog(logits, device="cuda", buckets=10, low=-3, high=3)
 
-for i in range(200):
+for i in range(10000):
     if i % 8 == 0:
         ground = torch.tensor([-0.5]).to("cuda")
     elif i % 23 == 1:
@@ -21,5 +21,5 @@ for i in range(200):
     loss = -dist.log_prob(ground).mean()
     loss.backward()
     opt.step()
-    if i % 10 == 0:
+    if i % 200 == 0:
         print(logits)
