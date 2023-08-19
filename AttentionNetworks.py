1e9aa4cecd53fb989f2ab79953c0bd39635d50fd
shell769324 <bobbyzenpai@gmail.com>
2023-08-19 13:26:52 -0400

    Add predicted reward

--- AttentionNetworks.py
+++ AttentionNetworks.py
@@ -216,5 +216,6 @@ class A2C(nn.Module):
             out = out.reshape(original[0], original[1], -1)
         actions = self._action_layer(out)
         values = self._value_layer(out)
+        print("a2c", actions[0], values[0])
 
         return values, actions
