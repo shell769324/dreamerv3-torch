49f0c1511eba85574db3009caa719922dac70733
shell769324 <bobbyzenpai@gmail.com>
2023-08-19 13:42:54 -0400

    Add predicted reward

--- dreamer.py
+++ dreamer.py
@@ -357,7 +357,7 @@ def main(config, defaults):
         agent._should_pretrain._once = False
 
     state = None
-    with wandb.init(project='mastering crafter with world models', config=defaults):
+    with wandb.init(project='mastering crafter with world models', config=defaults, id="6fsdh5va", resume=True):
         while agent._step < config.steps:
             print("Start training.")
             state = tools.simulate(agent, train_env, train_crafter, config.eval_every, state=state, metrics=agent._metrics)
