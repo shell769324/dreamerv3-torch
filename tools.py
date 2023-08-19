47494c37ff414435534e045351d7702c78ff2e00
shell769324 <bobbyzenpai@gmail.com>
2023-08-18 21:06:36 -0400

    Let's try

--- tools.py
+++ tools.py
@@ -617,14 +617,6 @@ class Optimizer:
         norms = {}
         for k, v in self._sub.items():
             if k == "reward":
-                print("stoch", torch.nn.utils.clip_grad_norm_(v.stoch_layer.parameters(), self._clip).item())
-                print("deter", torch.nn.utils.clip_grad_norm_(v.deter_layer.parameters(), self._clip).item())
-                print("layers", torch.nn.utils.clip_grad_norm_(v.layers.parameters(), self._clip).item())
-                print("mean_layer", torch.nn.utils.clip_grad_norm_(v.mean_layer.parameters(), self._clip).item())
-                print("mean_layer avg", v.mean_layer.weight.detach().abs().mean().item())
-                for param in v.parameters():
-                    if param.grad is not None:
-                        print("param grad", param.grad.shape, param.grad)
                 print("stoch", v.stoch_layer.parameters().shape, "\n", v.stoch_layer.parameters().grad)
                 print("deter", v.deter_layer.parameters().shape, "\n", v.deter_layer.parameters().grad)
                 for i, l in enumerate(v.layers()):
@@ -635,8 +627,9 @@ class Optimizer:
                     for param in l.parameters():
                         if param.grad is not None:
                             print(param.grad.shape, param.grad)
-            else:
-                norms[k] = torch.nn.utils.clip_grad_norm_(v.parameters(), self._clip)
+                print("mean", v.mean_layer.parameters().shape, "\n", v.mean_layer.parameters().grad)
+                print("std", v.std_layer.parameters().shape, "\n", v.std_layer.parameters().grad)
+            norms[k] = torch.nn.utils.clip_grad_norm_(v.parameters(), self._clip)
         self._scaler.step(self._opt)
         self._scaler.update()
         # self._opt.step()
