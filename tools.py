4f836fd6cfce8e60f6b06a882e512d8bcc9075c0
shell769324 <bobbyzenpai@gmail.com>
2023-08-18 20:51:07 -0400

    Let's try

--- tools.py
+++ tools.py
@@ -622,8 +622,8 @@ class Optimizer:
                 print("layers", torch.nn.utils.clip_grad_norm_(v.layers.parameters(), self._clip).item())
                 print("mean_layer", torch.nn.utils.clip_grad_norm_(v.mean_layer.parameters(), self._clip).item())
                 print("mean_layer avg", v.mean_layer.weight.detach().abs().mean().item())
-                for param in v.mean_layer.parameters():
-                    print("mean param grad", param.grad.abs().mean().item())
+                for param in v.parameters():
+                    print("param grad", param.grad.abs().mean().item())
             else:
                 norms[k] = torch.nn.utils.clip_grad_norm_(v.parameters(), self._clip)
         self._scaler.step(self._opt)
