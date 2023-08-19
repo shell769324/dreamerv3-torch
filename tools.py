0dd2ca1490fabcd13bd0d0aab75f1eeecc10411f
shell769324 <bobbyzenpai@gmail.com>
2023-08-18 20:11:54 -0400

    Let's try

--- tools.py
+++ tools.py
@@ -616,18 +616,7 @@ class Optimizer:
         self._scaler.unscale_(self._opt)
         norms = {}
         for k, v in self._sub.items():
-            if k == "reward":
-                print("stoch", torch.nn.utils.clip_grad_norm_(v.stoch_layer.parameters(), self._clip).item())
-                print("deter", torch.nn.utils.clip_grad_norm_(v.deter_layer.parameters(), self._clip).item())
-                print("layers", torch.nn.utils.clip_grad_norm_(v.layers.parameters(), self._clip).item())
-                print("mean_layer", torch.nn.utils.clip_grad_norm_(v.mean_layer.parameters(), self._clip).item())
-                print("mean_layer shape", v.mean_layer.weight.shape)
-                print("mean_layer avg", v.mean_layer.weight.detach().abs().mean().item())
-                print("mean_layer weight", v.mean_layer.weight)
-                for param in v.mean_layer.parameters():
-                    print("mean param grad", param.grad)
-            else:
-                norms[k] = torch.nn.utils.clip_grad_norm_(v.parameters(), self._clip)
+            norms[k] = torch.nn.utils.clip_grad_norm_(v.parameters(), self._clip)
         self._scaler.step(self._opt)
         self._scaler.update()
         # self._opt.step()
