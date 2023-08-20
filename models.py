a50128ed3a00cc3bd5c822cca28f80a1dd7f4cad
shell769324 <bobbyzenpai@gmail.com>
2023-08-20 14:47:09 -0400

    Go

--- models.py
+++ models.py
@@ -158,6 +158,8 @@ class WorldModel(nn.Module):
 
                     likes[name] = like
                     losses[name] = -torch.mean(like) * self._scales.get(name, 1.0)
+                    if name == "image":
+                        print(like)
                     if name == "reward":
                         for i in range(len(targets)):
                             conditional_metrics[targets[i] + "_" + name + "_diff"] = to_np(
