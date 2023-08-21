e629788f4dbca3ec16c6abf268af48c736eed353
shell769324 <bobbyzenpai@gmail.com>
2023-08-21 00:51:34 -0400

    Go

--- models.py
+++ models.py
@@ -159,13 +159,13 @@ class WorldModel(nn.Module):
                     likes[name] = like
                     if name == "image":
                         print("likelihood", like[..., 2:])
-                        loss = -torch.mean(like) * self._scales.get(name, 1.0)
-                        # losses[name] = -torch.mean(like) * self._scales.get(name, 1.0)
+                        # loss = -torch.mean(like) * self._scales.get(name, 1.0)
+                        losses[name] = -torch.mean(like) * self._scales.get(name, 1.0)
                         # losses[name].backward()
-                        self._model_opt._scaler.scale(loss).backward()
-                        print(name, loss)
-                        print("mode grad", pred._mode, pred._mode.shape)
-                        self._model_opt.temp("image", self.heads["image"])
+                        # self._model_opt._scaler.scale(loss).backward()
+                        # print(name, loss)
+                        # print("mode grad", pred._mode, pred._mode.shape)
+                        # self._model_opt.temp("image", self.heads["image"])
                     else:
                         losses[name] = -torch.mean(like) * self._scales.get(name, 1.0)
                     if name == "reward":
