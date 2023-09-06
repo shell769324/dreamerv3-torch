0ae7761cb5f481c3ae9879a4ad25b51650459e23
shell769324 <bobbyzenpai@gmail.com>
2023-09-06 09:19:25 -0400

    Try hard

--- videoPlayer.py
+++ videoPlayer.py
@@ -2,9 +2,22 @@ import os
 import imageio
 import pathlib
 import numpy as np
+import os, shutil
 
 directory="/Users/nardis/Downloads/trajectories"
 directory = pathlib.Path(directory).expanduser()
+
+video_dir = "/Users/nardis/Downloads/videos"
+
+for filename in os.listdir(video_dir):
+    file_path = os.path.join(video_dir, filename)
+    try:
+        if os.path.isfile(file_path) or os.path.islink(file_path):
+            os.unlink(file_path)
+        elif os.path.isdir(file_path):
+            shutil.rmtree(file_path)
+    except Exception as e:
+        print('Failed to delete %s. Reason: %s' % (file_path, e))
 
 for filename in sorted(directory.glob("*.npz")):
     with filename.open("rb") as f:
