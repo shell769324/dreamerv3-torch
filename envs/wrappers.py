63ba337f11dbb8d9c8e218206442a6a08398ffbb
shell769324 <bobbyzenpai@gmail.com>
2023-10-07 22:55:17 -0400

    go

--- envs/wrappers.py
+++ envs/wrappers.py
@@ -40,8 +40,6 @@ class CollectDataset:
         if transition["reward_mode"] != self._episode[-1]["reward_mode"] or done:
             # mode 0 is nagivate
             ep_name = str(get_episode_name(self.directory))
-            print(ep_name, type(ep_name))
-            exit(1)
             dataset = [self.navigate_dataset, self.explore_dataset][self._episode[-1]["reward_mode"]]
             cache = dataset.tuples
             if ep_name not in cache[self._episode[-1]["target"]]:
