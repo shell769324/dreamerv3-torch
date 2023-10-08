6c7230dc46ce9b161db1a71e9003aa2fcce5d158
shell769324 <bobbyzenpai@gmail.com>
2023-10-07 23:00:39 -0400

    go

--- dreamer.py
+++ dreamer.py
@@ -397,10 +397,11 @@ def main(config, defaults):
         agent._should_pretrain._once = False
 
     state = None
-    watched = [(agent._wm.heads["reward"], "reward.", 6000),
+    watched = [(agent._wm.heads["explore_reward"], "explore_reward.", 6000),
+               (agent._wm.heads["navigate_reward"], "navigate_reward.", 6000),
                (agent._wm.heads["image"], "image.", 2000),
-               (agent._task_behavior.a2c_navigate, "a2c.", 15000),
-               (agent._task_behavior.a2c_explore, "a2c.", 15000)]
+               (agent._task_behavior.a2c_navigate, "a2c_navigate.", 15000),
+               (agent._task_behavior.a2c_explore, "a2c_explore.", 15000)]
     # with wandb.init(project='mastering crafter with world models', config=defaults, id="nf5cv15k", resume=True):
     with wandb.init(project='mastering crafter with world models', config=defaults):
         for model, name, param_freq in watched:
