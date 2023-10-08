de54d15738c01243fc8e6043a5bffa7597ccbb36
shell769324 <bobbyzenpai@gmail.com>
2023-10-07 23:07:22 -0400

    go

--- tools.py
+++ tools.py
@@ -228,7 +228,7 @@ class SliceDataset:
         remained = self.batch_length * self.batch_size - sum(frame_counts)
         frame_counts[random.randint(0, len(targets) - 1)] += remained
         tuple_list = [list(self.tuples[i].items()) for i in range(len(targets))]
-        p = [self.episode_sizes[i] for i in range(len(targets))]
+        p = [np.array([self.episode_sizes[i][name] for name, _ in tuple_list[i]]) for i in range(len(targets))]
         # sample episode by their length
         for i, sl in enumerate(p):
             p[i] = sl / np.sum(sl)
