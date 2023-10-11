9ce2acad52ec77dbc1ad6440cf56d43b4b4847ac
shell769324 <bobbyzenpai@gmail.com>
2023-10-10 21:26:07 -0400

    go

--- models.py
+++ models.py
@@ -152,7 +152,7 @@ class WorldModel(nn.Module):
         target_dist = target_dist / np.sum(target_dist)
         navigate_data = self.preprocess(self.navigate_dataset.sample(target_dist))
         explore_data = self.preprocess(self.explore_dataset.sample(target_dist))
-        data = {k: torch.cat([v, explore_data[v]], dim=0) for k, v in navigate_data.items() if k in explore_data}
+        data = {k: torch.cat([v, explore_data[k]], dim=0) for k, v in navigate_data.items() if k in explore_data}
 
         with tools.RequiresGrad(self):
             with torch.cuda.amp.autocast(self._use_amp):
