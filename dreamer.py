a21e59e1e6c3a31bd7dfa5466139bb9ddc5aeccb
shell769324 <bobbyzenpai@gmail.com>
2023-10-12 19:09:02 -0400

    go

--- dreamer.py
+++ dreamer.py
@@ -165,12 +165,13 @@ class Dreamer(nn.Module):
         for i, target in enumerate(obs["target"]):
             target_array[i] = target.to(self._config.device)
         stoch, deter = self._wm.dynamics.get_sep(latent)
-        reward_prediction = self._wm.heads["reward"](stoch.unsqueeze(0), deter.unsqueeze(0), target_array)
         crafter_env = self.train_crafter if training else self.eval_crafter
         if obs["target_spot"]:
+            reward_prediction = self._wm.heads["navigate_reward"](stoch.unsqueeze(0), deter.unsqueeze(0), target_array)
             means, policy_params = self._task_behavior.a2c_navigate(stoch, deter, target_array)
             crafter_env.reward_type = "navigate"
         else:
+            reward_prediction = self._wm.heads["explore_reward"](stoch.unsqueeze(0), deter.unsqueeze(0), target_array)
             means, policy_params = self._task_behavior.a2c_explore(stoch, deter, target_array)
             crafter_env.reward_type = "explore"
         actor = tools.OneHotDist(policy_params, unimix_ratio=self._config.action_unimix_ratio)
