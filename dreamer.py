69bc68c26125fb32d798903840a47f06cea05427
shell769324 <bobbyzenpai@gmail.com>
2023-10-22 22:29:58 -0400

    go

--- dreamer.py
+++ dreamer.py
@@ -186,7 +186,7 @@ class Dreamer(nn.Module):
             means, policy_params = self._task_behavior.a2c_explore(stoch, deter, target_array)
             crafter_env.reward_type = "explore"
         where_prediction = self._wm.heads["where"](self._wm.dynamics.get_feat(latent))
-        crafter_env.predicted_where = where_prediction.mode()
+        crafter_env.predicted_where = where_prediction.mode().to(torch.long)
         print(crafter_env.predicted_where)
         exit(1)
         actor = tools.OneHotDist(policy_params, unimix_ratio=self._config.action_unimix_ratio)
