f72795d5a28ffcc48621d902c2882c7c73198976
shell769324 <bobbyzenpai@gmail.com>
2023-10-26 09:37:43 -0400

    go

--- models.py
+++ models.py
@@ -175,7 +175,7 @@ class WorldModel(nn.Module):
                 where_like = where_pred.log_prob(data["where"].reshape(data["where"].shape[:-2] + (np.prod(data["where"].shape[-2:]),)))
                 print(front_pred.mode().shape)
                 print(data["front"].shape)
-                front_like = front_pred.log_prob(torch.nn.functional.one_hot(data["front"], num_classes=len(targets + 1)).to(self._config.device))
+                front_like = front_pred.log_prob(torch.nn.functional.one_hot(data["front"], num_classes=len(targets) + 1).to(self._config.device))
                 losses["where"] = -torch.mean(where_like) * self._scales.get("where", 1.0)
                 losses["front"] = -torch.mean(front_like) * self._scales.get("front", 1.0)
 
