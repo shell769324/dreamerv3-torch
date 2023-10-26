7459e0b7913dba01a0a2bb5823b916621394c9b4
shell769324 <bobbyzenpai@gmail.com>
2023-10-26 09:27:39 -0400

    go

--- models.py
+++ models.py
@@ -172,7 +172,7 @@ class WorldModel(nn.Module):
                 explore_embed = self.encoder(explore_data)
                 combined_embed = torch.cat([navigate_embed, explore_embed], 0)
                 where_pred, front_pred = self.embed_where(combined_embed)
-                where_like = where_pred.log_prob(data["where"].reshape(-1))
+                where_like = where_pred.log_prob(data["where"].reshape(data["where"].shape[:-2] + (np.prod(data["where"].shape[-2:]))))
                 front_like = front_pred.log_prob(torch.nn.functional.one_hot(data["front"], num_classes=len(targets + 1)).to(self._config.device))
                 losses["where"] = -torch.mean(where_like) * self._scales.get("where", 1.0)
                 losses["front"] = -torch.mean(front_like) * self._scales.get("front", 1.0)
