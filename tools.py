04077fda57d103535c4acf8fc71012a92b5883dc
shell769324 <bobbyzenpai@gmail.com>
2023-10-26 09:34:17 -0400

    go

--- tools.py
+++ tools.py
@@ -273,7 +273,10 @@ class SliceDataset:
         for _ in range(self.batch_size):
             size = 0
             while size < self.batch_length:
-                picked = self.random.choice(list(range(len(tuple_list[curr_target]))), p=p[curr_target])
+                try:
+                    picked = self.random.choice(list(range(len(tuple_list[curr_target]))), p=p[curr_target])
+                except:
+                    print(curr_target, tuple_list)
                 (ep_name, slices_in_episode) = tuple_list[curr_target][picked]
                 episode = self.dataset[ep_name]
                 num_slices = self.episode_sizes[curr_target][ep_name]
