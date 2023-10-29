e103835f037cd96aea717811ae0dc6673a4f0ff3
shell769324 <bobbyzenpai@gmail.com>
2023-10-29 15:27:29 -0400

    go

--- tools.py
+++ tools.py
@@ -252,10 +252,11 @@ class SliceDataset:
                         reward_mode = 0 if self.name == "navigate" else 1
                         assert self.dataset[ep_name]["reward_mode"][t] == reward_mode, "{} {}: {} transition {} {} reward_mode wrong". \
                             format(self.mode, self.name, ep_name, t, "navigate" if self.dataset[ep_name]["reward_mode"][t] == 0 else "explore")
-                    assert self.dataset[ep_name]["prev_target"][ed - 1] == i, "{} {}: {} transition {} is {}, not {}\n{}\n{}\n{}\n{}". \
-                        format(self.mode, self.name, ep_name, ed - 1, targets[self.dataset[ep_name]["prev_target"][ed - 1]],
-                               targets[i], self.dataset[ep_name]["prev_target"], self.dataset[ep_name]["target"], self.dataset[ep_name]["reward_mode"],
-                               self.tuples[i][ep_name])
+                    if ed != len(self.dataset[ep_name]["target"]):
+                        assert self.dataset[ep_name]["prev_target"][ed - 1] == i, "{} {}: {} transition {} is {}, not {}\n{}\n{}\n{}\n{}". \
+                            format(self.mode, self.name, ep_name, ed - 1, targets[self.dataset[ep_name]["prev_target"][ed - 1]],
+                                   targets[i], self.dataset[ep_name]["prev_target"], self.dataset[ep_name]["target"], self.dataset[ep_name]["reward_mode"],
+                                   self.tuples[i][ep_name])
                 assert total == count, "{} {}: expected total for {} {} is {}, actual is {}".format(self.mode, self.name, ep_name, targets[i], count, total)
         for i in range(len(targets)):
             for ep_name in self.tuples[i].keys():
