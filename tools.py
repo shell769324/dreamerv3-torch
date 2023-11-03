007b3c860e6d96d40c01a58c02d08ab78c819205
shell769324 <bobbyzenpai@gmail.com>
2023-11-03 10:40:50 -0400

    go

--- tools.py
+++ tools.py
@@ -295,8 +295,10 @@ class SliceDataset:
     def subsample(self, ret, markers, dist, batch_size, tuples, episode_sizes, aggregate_sizes):
         sufa = "success" if tuples == self.success_tuples else "failure"
         frame_counts = [0.0] * len(targets)
+        print(dist)
         dist = np.where(np.array(aggregate_sizes) == 0, 0, dist)
         dist = dist / np.sum(dist)
+        print(aggregate_sizes)
         print(self.name, sufa, dist)
         for i in range(len(dist)):
             frame_counts[i] = math.floor(self.batch_length * batch_size * dist[i])
