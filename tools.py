46592f561496f77bce864dc2d9468082709b63c4
shell769324 <bobbyzenpai@gmail.com>
2023-11-04 09:27:45 -0400

    go

--- tools.py
+++ tools.py
@@ -435,6 +435,7 @@ class SliceDataset:
                     self.failure_tuples[target][ep_name].append([start, len(episode["target"])])
                     self.failure_episode_sizes[target][ep_name] += len(episode["target"]) - start
                     self.failure_aggregate_sizes[target] += len(episode["target"]) - start
+            exit(1)
             self.save()
             self.sanity_check()
 
