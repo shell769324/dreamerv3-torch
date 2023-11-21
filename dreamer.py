fed82b9ffa10614baaa7ef6a0bfd4b22e60704d5
shell769324 <bobbyzenpai@gmail.com>
2023-11-21 16:23:49 -0500

    go

--- dreamer.py
+++ dreamer.py
@@ -486,6 +486,7 @@ def main(config, defaults):
             logprob = random_actor.log_prob(action)
             crafter_env = train_crafter if training else eval_crafter
             crafter_env.reward_type = actor_mode_list[o["actor_mode"][0]]
+            print("random agent in action")
             return {"action": action, "logprob": logprob}, None
 
         tools.simulate(random_agent, train_env, train_crafter, prefill)
