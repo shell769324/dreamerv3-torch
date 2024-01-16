f82a796ef0d84f3724b303e772c2310760839231
Bobby Zhang <huizhz@amazon.com>
2024-01-16 00:54:47 -0500

    Create agent

--- experiment.py
+++ experiment.py
@@ -1,12 +1,15 @@
-import json
 import os
 import sys
 
 import boto3
-import botocore
 
 module_path = ".."
 sys.path.append(os.path.abspath(module_path))
 
-client = boto3.client('bedrock')
-print(client.list_foundation_models())
+client = boto3.client('bedrock-agent')
+info = client.create_agent(agentName="tester",
+                           agentResourceRoleArn="arn:aws:iam::605719699353:role/rookery",
+                           foundationModel="arn:aws:bedrock:us-east-1::foundation-model/anthropic.claude-v2:1:200k",
+                           instruction="You will play a survival game with the goal to get diamond.",
+                           idleSessionTTLinSeconds=1800)
+print(info['agent'])
