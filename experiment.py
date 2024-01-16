a2755c0346af3533e668642a9039e0d157a935c8
Bobby Zhang <huizhz@amazon.com>
2024-01-16 00:55:24 -0500

    case issue in experiment

--- experiment.py
+++ experiment.py
@@ -11,5 +11,5 @@ info = client.create_agent(agentName="tester",
                            agentResourceRoleArn="arn:aws:iam::605719699353:role/rookery",
                            foundationModel="arn:aws:bedrock:us-east-1::foundation-model/anthropic.claude-v2:1:200k",
                            instruction="You will play a survival game with the goal to get diamond.",
-                           idleSessionTTLinSeconds=1800)
+                           idleSessionTTLInSeconds=1800)
 print(info['agent'])
