d42c59b83f02b28f48f824a05d77d1ec395aa8f1
Bobby Zhang <huizhz@amazon.com>
2024-01-17 09:43:57 -0500

    Alias agent

--- experiment.py
+++ experiment.py
@@ -6,19 +6,42 @@ import boto3
 module_path = ".."
 sys.path.append(os.path.abspath(module_path))
 
-sts_client = boto3.client("sts")
-assume_role_result = sts_client.assume_role(
-    RoleArn="arn:aws:iam::605719699353:role/AmazonBedrockExecutionRoleForAgents_rookery",
-    RoleSessionName="bedrock_test",
-    DurationSeconds=900)
 
-client = boto3.client('bedrock-agent',
-                      aws_access_key_id=assume_role_result['Credentials']['AccessKeyId'],
-                      aws_secret_access_key=assume_role_result['Credentials']['SecretAccessKey'],
-                      aws_session_token=assume_role_result['Credentials']['SessionToken'])
-info = client.create_agent(agentName="tester",
-                           agentResourceRoleArn="arn:aws:iam::605719699353:role/AmazonBedrockExecutionRoleForAgents_rookery",
-                           foundationModel="anthropic.claude-v2:1",
-                           instruction="You will play a survival game with the goal to get diamond.",
-                           idleSessionTTLInSeconds=1800)
-print(info['agent'])
+def create_bedrock_client():
+    sts_client = boto3.client("sts")
+    assume_role_result = sts_client.assume_role(
+        RoleArn="arn:aws:iam::605719699353:role/AmazonBedrockExecutionRoleForAgents_rookery",
+        RoleSessionName="bedrock_test",
+        DurationSeconds=900)
+    return boto3.client('bedrock-agent',
+                        aws_access_key_id=assume_role_result['Credentials']['AccessKeyId'],
+                        aws_secret_access_key=assume_role_result['Credentials']['SecretAccessKey'],
+                        aws_session_token=assume_role_result['Credentials']['SessionToken'])
+
+
+def create_agent(client):
+    return client.create_agent(
+        agentName="tester",
+        agentResourceRoleArn="arn:aws:iam::605719699353:role/AmazonBedrockExecutionRoleForAgents_rookery",
+        foundationModel="anthropic.claude-v2:1",
+        instruction="You will play a survival game with the goal to get diamond.",
+        idleSessionTTLInSeconds=1800)
+
+
+def alias_agent(client, alias):
+    info = client.create_agent_alias(
+        agentId="8SQUSFV9XU",
+        agentAliasName=alias
+    )
+    print(info)
+
+
+def call_agent(alias="tester1"):
+    client = boto3.client('bedrock-agent-runtime')
+    client.invoke_agent(
+        agentId="8SQUSFV9XU",
+
+    )
+
+
+alias_agent(create_bedrock_client(), "tester1")
