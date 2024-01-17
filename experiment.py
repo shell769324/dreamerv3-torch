5a061c7d0f50cb3bf1632db1bda8f444a8e686fd
Bobby Zhang <huizhz@amazon.com>
2024-01-16 20:34:45 -0500

    Add session token

--- experiment.py
+++ experiment.py
@@ -14,7 +14,8 @@ assume_role_result = sts_client.assume_role(
 
 client = boto3.client('bedrock-agent',
                       aws_access_key_id=assume_role_result['Credentials']['AccessKeyId'],
-                      aws_secret_access_key=assume_role_result['Credentials']['SecretAccessKey'])
+                      aws_secret_access_key=assume_role_result['Credentials']['SecretAccessKey'],
+                      aws_session_token=assume_role_result['Credentials']['SessionToken'])
 info = client.create_agent(agentName="tester",
                            agentResourceRoleArn="arn:aws:iam::605719699353:role/AmazonBedrockExecutionRoleForAgents_rookery",
                            foundationModel="arn:aws:bedrock:us-east-1::foundation-model/anthropic.claude-v2:1:200k",
