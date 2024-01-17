56b6f48ffa2c3e3fd69c3734506cf60fbeb143fa
Bobby Zhang <huizhz@amazon.com>
2024-01-16 20:31:50 -0500

    Update bedrock iam role

--- experiment.py
+++ experiment.py
@@ -6,9 +6,17 @@ import boto3
 module_path = ".."
 sys.path.append(os.path.abspath(module_path))
 
-client = boto3.client('bedrock-agent')
+sts_client = boto3.client("sts")
+assume_role_result = sts_client.assume_role(
+    RoleArn="arn:aws:iam::605719699353:role/AmazonBedrockExecutionRoleForAgents_rookery",
+    RoleSessionName="bedrock_test",
+    DurationSeconds="900")
+
+client = boto3.client('bedrock-agent',
+                      aws_access_key_id=assume_role_result['Credentials']['AccessKeyId'],
+                      aws_secret_access_key=assume_role_result['Credentials']['SecretAccessKey'])
 info = client.create_agent(agentName="tester",
-                           agentResourceRoleArn="arn:aws:iam::605719699353:role/rookery",
+                           agentResourceRoleArn="arn:aws:iam::605719699353:role/AmazonBedrockExecutionRoleForAgents_rookery",
                            foundationModel="arn:aws:bedrock:us-east-1::foundation-model/anthropic.claude-v2:1:200k",
                            instruction="You will play a survival game with the goal to get diamond.",
                            idleSessionTTLInSeconds=1800)
